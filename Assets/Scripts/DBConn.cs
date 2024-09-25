6aaff19a388a2b0886c43127883e9bff2f62572b
Kavya Adhikari <adhikarikavya1995@gmail.com>
2024-09-25 12:07:57 +0100

    Database connection

--- Assets/Scripts/DBConn.cs
+++ Assets/Scripts/DBConn.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+// UnityWebRequest.Get example
+
+// Access a website and use UnityWebRequest.Get to download a page.
+// Also try to download a non-existing page. Display the error.
 
 public class DBConn : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // A correct website page.
+        StartCoroutine(GetRequest("http://localhost/hearthub/dbconn.php"));
+
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator GetRequest(string uri)
     {
-        
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + webRequest.error);
+            }
+            else
+            {
+                // Read and display the response from the PHP file
+                string response = webRequest.downloadHandler.text;
+                Debug.Log("Response from PHP: " + response);
+            }
+
+
+        }
     }
 }
